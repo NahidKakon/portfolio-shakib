01aee7f03c3117aca8c1824e2c69b57883cc7ae2
NahidKakon <nahidshakib@outlook.com>
2024-10-06 13:15:31 +0100

    Update aboutme.tsx

--- src/app/pages/aboutme.tsx
+++ src/app/pages/aboutme.tsx
@@ -2,17 +2,38 @@ import React from "react";
 
 export default function AboutMe() {
     return(
-
-  <>
- <img   src="https://vercel.com/nahids-projects-a2eed867/~/stores/blob/store_UhIExemgiHHBfkeK/browser?file_url=https%253A%252F%252Fuhiexemgihhbfkek.public.blob.vercel-storage.com%252Fkey_skills-kanPzLU5gjLRwsNGO4P6jWmd36YO1a.jpg"
- style={{ width: "100%" }} />
- <img src="https://vercel.com/nahids-projects-a2eed867/~/stores/blob/store_UhIExemgiHHBfkeK/browser?file_url=https%253A%252F%252Fuhiexemgihhbfkek.public.blob.vercel-storage.com%252Faboutme%252Fcareer-ns5YsyJjwHh3swcaRhHulRmADbxPQj.jpg"
-style={{ width: "100%" }} />
- <img src="https://vercel.com/nahids-projects-a2eed867/~/stores/blob/store_UhIExemgiHHBfkeK/browser?file_url=https%253A%252F%252Fuhiexemgihhbfkek.public.blob.vercel-storage.com%252FcredlyBadge-9P5Y9W8nHEb3d2B16cUbkbveNmrfqQ.png"
- style={{ width: "100%" }} />
- <img src="https://vercel.com/nahids-projects-a2eed867/~/stores/blob/store_UhIExemgiHHBfkeK/browser?file_url=https%253A%252F%252Fuhiexemgihhbfkek.public.blob.vercel-storage.com%252Fcertificate-Oe9orGDMCTmTJ20g99bbU1FAFw4iA7.jpg"
- style={{ width: "100%" }} />
- </>
-
+<>
+  <div className="gallery">
+    <a target="_blank" href=""https://vercel.com/nahids-projects-a2eed867/~/stores/blob/store_UhIExemgiHHBfkeK/browser?file_url=https%253A%252F%252Fuhiexemgihhbfkek.public.blob.vercel-storage.com%252Fkey_skills-kanPzLU5gjLRwsNGO4P6jWmd36YO1a.jpg"
+">
+      <img src=""https://vercel.com/nahids-projects-a2eed867/~/stores/blob/store_UhIExemgiHHBfkeK/browser?file_url=https%253A%252F%252Fuhiexemgihhbfkek.public.blob.vercel-storage.com%252Fkey_skills-kanPzLU5gjLRwsNGO4P6jWmd36YO1a.jpg"
+" alt="Cinque Terre" width={600} height={400} />
+    </a>
+    <div className="desc"></div>
+  </div>
+  <div className="gallery">
+    <a target="_blank" href="https://vercel.com/nahids-projects-a2eed867/~/stores/blob/store_UhIExemgiHHBfkeK/browser?file_url=https%253A%252F%252Fuhiexemgihhbfkek.public.blob.vercel-storage.com%252Faboutme%252Fcareer-ns5YsyJjwHh3swcaRhHulRmADbxPQj.jpg">
+      <img src="https://vercel.com/nahids-projects-a2eed867/~/stores/blob/store_UhIExemgiHHBfkeK/browser?file_url=https%253A%252F%252Fuhiexemgihhbfkek.public.blob.vercel-storage.com%252Faboutme%252Fcareer-ns5YsyJjwHh3swcaRhHulRmADbxPQj.jpg" alt="Forest" width={600} height={400} />
+    </a>
+    <div className="desc"></div>
+  </div>
+  <div className="gallery">
+    <a target="_blank" href="https://vercel.com/nahids-projects-a2eed867/~/stores/blob/store_UhIExemgiHHBfkeK/browser?file_url=https%253A%252F%252Fuhiexemgihhbfkek.public.blob.vercel-storage.com%252FcredlyBadge-9P5Y9W8nHEb3d2B16cUbkbveNmrfqQ.png">
+      <img
+        src="https://vercel.com/nahids-projects-a2eed867/~/stores/blob/store_UhIExemgiHHBfkeK/browser?file_url=https%253A%252F%252Fuhiexemgihhbfkek.public.blob.vercel-storage.com%252FcredlyBadge-9P5Y9W8nHEb3d2B16cUbkbveNmrfqQ.png"
+        alt="Northern Lights"
+        width={600}
+        height={400}
+      />
+    </a>
+    <div className="desc"></div>
+  </div>
+  <div className="gallery">
+    <a target="_blank" href="https://vercel.com/nahids-projects-a2eed867/~/stores/blob/store_UhIExemgiHHBfkeK/browser?file_url=https%253A%252F%252Fuhiexemgihhbfkek.public.blob.vercel-storage.com%252Fcertificate-Oe9orGDMCTmTJ20g99bbU1FAFw4iA7.jpg">
+      <img src="https://vercel.com/nahids-projects-a2eed867/~/stores/blob/store_UhIExemgiHHBfkeK/browser?file_url=https%253A%252F%252Fuhiexemgihhbfkek.public.blob.vercel-storage.com%252Fcertificate-Oe9orGDMCTmTJ20g99bbU1FAFw4iA7.jpg" alt="Mountains" width={600} height={400} />
+    </a>
+    <div className="desc"></div>
+  </div>
+</>
     )
 }
