35cf0793f7b5cf6f7579f400c370af0500a0a541
NahidKakon <nahidshakib@outlook.com>
2024-10-06 10:20:14 +0100

    Update aboutme.tsx

--- src/app/pages/aboutme.tsx
+++ src/app/pages/aboutme.tsx
@@ -5,13 +5,13 @@ export default function AboutMe() {
 
   <>
  <img   src="https://vercel.com/nahids-projects-a2eed867/~/stores/blob/store_UhIExemgiHHBfkeK/browser?file_url=https%253A%252F%252Fuhiexemgihhbfkek.public.blob.vercel-storage.com%252Fkey_skills-kanPzLU5gjLRwsNGO4P6jWmd36YO1a.jpg"
- alt="Paris" className="center" />
+ style={{ width: "100%" }} />
  <img src="https://vercel.com/nahids-projects-a2eed867/~/stores/blob/store_UhIExemgiHHBfkeK/browser?file_url=https%253A%252F%252Fuhiexemgihhbfkek.public.blob.vercel-storage.com%252Faboutme%252Fcareer-ns5YsyJjwHh3swcaRhHulRmADbxPQj.jpg"
- alt="Paris" className="center" />
+style={{ width: "100%" }} />
  <img src="https://vercel.com/nahids-projects-a2eed867/~/stores/blob/store_UhIExemgiHHBfkeK/browser?file_url=https%253A%252F%252Fuhiexemgihhbfkek.public.blob.vercel-storage.com%252FcredlyBadge-9P5Y9W8nHEb3d2B16cUbkbveNmrfqQ.png"
- alt="Paris" className="center" />
+ style={{ width: "100%" }} />
  <img src="https://vercel.com/nahids-projects-a2eed867/~/stores/blob/store_UhIExemgiHHBfkeK/browser?file_url=https%253A%252F%252Fuhiexemgihhbfkek.public.blob.vercel-storage.com%252Fcertificate-Oe9orGDMCTmTJ20g99bbU1FAFw4iA7.jpg"
- alt="Paris" className="center" />
+ style={{ width: "100%" }} />
  </>
 
     )
